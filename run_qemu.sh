e9cda34c04b806a36677379f99f8ac9be9e45786
Srimanta Barua <srimanta.barua1@gmail.com>
2020-11-18 18:37:43 -0500

    Update run_qemu script and add more events

--- run_qemu.sh
+++ run_qemu.sh
@@ -3,6 +3,7 @@
 DISK=archmod.qcow2
 SMP=2
 CDROM=""
+SHARED_FOLDER="$PWD/shared_folder/"
 #CDROM="-cdrom /home/barua/Downloads/archlinux-2020.09.01-x86_64.iso"
 
 qemu-system-x86_64 \
@@ -10,7 +11,7 @@ qemu-system-x86_64 \
     -enable-kvm \
     -smp $SMP \
     -m 2048 \
-    -nic user,model=virtio \
     -drive "file=$DISK,media=disk,if=virtio" \
+    -net nic -net "user,smb=$SHARED_FOLDER" \
     $CDROM \
     -sdl
